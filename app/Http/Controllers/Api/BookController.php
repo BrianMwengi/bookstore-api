d7e313af7a91413996273612db2173806fcc1866
Brian Mwengi <mwengibrian@gmail.com>
2024-03-06 17:14:35 +0300

    Intial Commit

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Book::all(); // Get all books
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all()); // Create a new book
+        return response()->json($book, 201); 
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $book = Book::find($id);
+        if (!$book) { // Check if the book is found
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+        return $book;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $book = Book::find($id);
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }  
+        $book->update($request->all());
+        return response()->json($book, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $book = Book::find($id);
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        } 
+        $book->delete();
+        return response()->json(['message' => 'Book deleted'], 200); 
+    }
+}
+
+
